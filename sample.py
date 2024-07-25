74d13fddfdab954de1c823041cdef8436259af15
Zaaccckkkk <116949883+Zaaccckkkk@users.noreply.github.com>
2024-07-25 22:20:32 +0800

    Use github_sop.pdf in the sample.py

--- sample.py
+++ sample.py
@@ -1,1 +1,15 @@
+from detect_color import ColorDetector
+from image_with_bbox import PDFImageExtractor
+
+
+# Change the paths below to match your file locations
+image_path = "/Users/wuzhentian/Desktop/box_line.png"
+pdf_path = "/Users/wuzhentian/Desktop/github_sop.pdf"
+output_folder = "/Users/wuzhentian/Desktop/get_page_images"
+output_folder2 = "/Users/wuzhentian/Desktop/get_images_useful"
+
+ColorDetector(image_path).print_top_colors()
+color = ColorDetector(image_path).get_top_color()
+image_extractor = PDFImageExtractor(pdf_path, output_folder, output_folder2, reference_color=[color])
+image_dict = image_extractor.extract_images()
 
