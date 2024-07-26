d3a48941ae3ad1547c7ceaafc505a116dec2899c
Zhentian Wu <116949883+Zaaccckkkk@users.noreply.github.com>
2024-07-26 17:08:12 +0800

    Update run.py

--- run.py
+++ run.py
@@ -8,7 +8,11 @@ pdf_path = "/Users/yourname/Desktop/sample_github_sop.pdf"
 output_folder = "/Users/yourname/Desktop/get_page_images"
 output_folder2 = "/Users/yourname/Desktop/get_useful_images"
 
+# Print the top colors in your border line image
 ColorDetector(image_path).print_top_colors()
+
+# Get the most common color in the border line image
 color = ColorDetector(image_path).get_top_color()
+# Extract all the images in the tables
 image_extractor = PDFImageExtractor(pdf_path, output_folder, output_folder2, reference_color=[color])
 image_dict = image_extractor.extract_images()
