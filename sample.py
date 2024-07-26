df3d2b31cfa84f9fc4c0828c10977d99105ff3e2
Zaaccckkkk <116949883+Zaaccckkkk@users.noreply.github.com>
2024-07-26 11:46:41 +0800

    Update sample.py

--- sample.py
+++ sample.py
@@ -6,7 +6,7 @@ from image_with_bbox import PDFImageExtractor
 image_path = "/Users/wuzhentian/Desktop/box_line.png"
 pdf_path = "/Users/wuzhentian/Desktop/github_sop.pdf"
 output_folder = "/Users/wuzhentian/Desktop/get_page_images"
-output_folder2 = "/Users/wuzhentian/Desktop/get_images_useful"
+output_folder2 = "/Users/wuzhentian/Desktop/get_useful_images"
 
 ColorDetector(image_path).print_top_colors()
 color = ColorDetector(image_path).get_top_color()
